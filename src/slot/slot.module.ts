0a7c20d9f5c974990a9fd3c81317f5a57ec95e4f
Huỳnh Minh Hiển <22130080@st.hcmuaf.edu.vn>
2025-05-19 16:41:19 +0700

    feat(slot): define Slot entity with relationships and implement Slot service methods

--- src/slot/slot.module.ts
+++ src/slot/slot.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SlotService } from './slot.service';
 import { SlotController } from './slot.controller';
+import { Slot } from './entities/slot.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Slot])],
   controllers: [SlotController],
   providers: [SlotService],
+  exports: [SlotService]
 })
 export class SlotModule {}
--- src/warehouse/entities/warehouse.entity.ts
+++ src/warehouse/entities/warehouse.entity.ts
@@ -1,1 +1,25 @@
-export class Warehouse {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('warehouse')
+export class Warehouse {
+  @PrimaryGeneratedColumn()
+  warehouseId: number;
+
+  @Column()
+  warehouseType: string;
+
+  @Column()
+  location: string;
+
+  @Column()
+  acreage: string;
+
+  @Column()
+  capacity: number;
+
+  constructor(partial?: Partial<Warehouse>) {
+    if (partial) {
+      Object.assign(this, partial);
+    }
+  }
+}
