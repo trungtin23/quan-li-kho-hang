df4c08e03b4731b1d2801c744a7e4f0dee15f3ec
Your Name <your.email@example.com>
2025-05-19 16:40:10 +0700

    feat(app): configure TypeORM for MySQL database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,17 @@ import { CarrierModule } from './carrier/carrier.module';
 import { TransactionDetailModule } from './transaction-detail/transaction-detail.module';
 import { WarehouseModule } from './warehouse/warehouse.module';
 import { SlotModule } from './slot/slot.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule, UserModule, SupplierModule, TransactionModule, CarrierModule, TransactionDetailModule, WarehouseModule, SlotModule],
+  imports: [ProductModule, UserModule, SupplierModule, TransactionModule, CarrierModule, TransactionDetailModule, WarehouseModule, SlotModule,
+    TypeOrmModule.forRoot({
+      url: 'mysql://root:@localhost:3306/Kho',     
+      type: 'mysql',                  
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,  
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
