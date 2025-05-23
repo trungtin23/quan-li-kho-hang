3d393acea045a4d3f152351057aac4cf646499f7
hien <you@example.com>
2025-05-23 23:12:40 +0700

    refactor: remove category from Product entity; update ProductRepository to include slot relations in findAll method; adjust Slot module import path

--- src/slot/slot.module.ts
+++ src/slot/slot.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SlotService } from './slot.service';
 import { SlotController } from './slot.controller';
-import { Slot } from './entities/slot.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Slot } from 'src/entities/slot.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Slot])],
