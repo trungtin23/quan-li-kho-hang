8280c0445ffcea0bdef6bb53f35491245b91fb33
Huỳnh Minh Hiển <22130080@st.hcmuaf.edu.vn>
2025-05-24 23:21:18 +0700

    fix: update CORS configuration to specify allowed origins and methods

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true, 
-    credentials: true, 
+     origin: [
+      'http://localhost:5173', 
+      'https://fronentquanlikhohang.vercel.app/',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
   });
   await app.listen(process.env.PORT ?? 3000);
 }
